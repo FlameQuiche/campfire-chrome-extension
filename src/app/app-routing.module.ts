03e0789823a87ec716fb4dac1cdb611df0001e61
Julien Herubel <jherubel@ippon.fr>
2018-11-06 09:23:10 +0100

    Adding domains, home / login components and routerModule

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HomeComponent }       from './home/home.component';
+import { LoginComponent }       from './login/login.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent }
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }    from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+
 import { AppService } from './app.service';
 
-import { AppComponent } from './app.component';
+import { AppRoutingModule }     from './app-routing.module';
 
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent }   from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [AppService],
   bootstrap: [AppComponent]
